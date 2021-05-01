1cc996fcfbd7d77640f3e16e61931ac8f51a1f12
Ryomasao <konoemario@gmail.com>
2021-05-01 16:32:26 +0900

    base作成

---
+++ components/orgs/Header/Header.tsx
@@ -0,0 +1,9 @@
+import style from './Header.module.scss'
+
+export default function Header() {
+  return (
+    <header>
+      <div className={style.container}>header</div>
+    </header>
+  )
+}
---
+++ components/pages/LP/FooContent.tsx
@@ -0,0 +1,9 @@
+export default function FooContent() {
+  return (
+    <section>
+      <ul>
+        <li>hey</li>
+      </ul>
+    </section>
+  )
+}
